6b0cd5de56d264a5303e4e55395eb8d3e86eb690
saronic <saronic@yeah.net>
2014-08-08 17:08:10 +0800

    Revert "鍙?浠ユ挱鏀緒av"
    This reverts commit 4c67132e083695069b42345f23c7db386baa36ce.
    
    Conflicts:
    	src/fei/exercise/android/hellomoon/AudioPlayer.java
    	src/fei/exercise/android/hellomoon/HelloMoonFragment.java

--- src/fei/exercise/android/hellomoon/HelloMoonFragment.java
+++ src/fei/exercise/android/hellomoon/HelloMoonFragment.java
@@ -4,42 +4,20 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class HelloMoonFragment extends Fragment {
 	private Button mPlayButton;
 	private Button mStopButton;
-	private AudioPlayer mPlayer = new AudioPlayer();
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
 		mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
-		mPlayButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mPlayer.play(getActivity());
-			}
-		});
-		
 		mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
-		mStopButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mPlayer.pause();
-			}
-		});
+		
 		return v;
-	}
-	
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		mPlayer.stop();
 	}
 }
